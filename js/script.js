12156e80d87bddb22e3ccf2ef42c37573d1a8fc1
marvinblaser <marvinblaser.pro@gmail.com>
2024-04-10 09:51:42 +0200

    adding sound + notes display

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 let tiles = document.querySelectorAll(".tile");
+let tileA = document.querySelector(".tile-a");
+let tileB = document.querySelector(".tile-b");
+let tileC = document.querySelector(".tile-c");
+let tileD = document.querySelector(".tile-d");
+let noteDo = document.querySelector("#do");
+let noteMi = document.querySelector("#mi");
+let noteFa = document.querySelector("#fa");
+let noteRe = document.querySelector("#re");
 let untouchable = document.querySelector(".untouchable");
 let start = document.querySelector(".start");
 let startButton = document.querySelector(".start-button");
@@ -20,4 +28,28 @@ tiles.forEach(tile =>{
 startButton.addEventListener("click", ()=>{
     start.remove();
     blurPage.remove();
-})
+    setTimeout(()=>{
+        playing();
+    }, "1000")
+});
+
+tileA.addEventListener("click", ()=>{
+    noteDo.play();
+});
+tileB.addEventListener("click", ()=>{
+    noteRe.play();
+});
+tileC.addEventListener("click", ()=>{
+    noteMi.play();
+});
+tileD.addEventListener("click", ()=>{
+    noteFa.play();
+});
+
+
+// do do do re do sol do do do re do sol
+// base do re mi fa so la si do
+// mi mi mi fa mi do | mi mi mi fa mi do
+function playing(){
+    
+}
